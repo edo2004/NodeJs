a72745fb9070bef213d1a38302ea8fc1985996af
edo2004 <53161686+edo2004@users.noreply.github.com>
2022-02-24 16:22:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 //Hello world by Eduar Ordoñez Muñoz
 
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World'
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
 // Hola mundo desde el usuario Eduar 
+
+
+const express = require("express"),
+    path = require("path"),
+    app = express(),
+    puerto = 3000;
+
+app.get('/', (peticion, respuesta) => {
+    // Podemos acceder a la petición HTTP
+    let agenteDeUsuario = peticion.header("user-agent");
+    respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
+});
+app.get('/pagina', (peticion, respuesta) => {
+    // Servir archivo HTML, o cualquier otro archivo
+    let rutaDeArchivo = path.join(__dirname, "plantilla.html");
+    respuesta.sendFile(rutaDeArchivo);
+});
+
+app.get('/hola', (peticion, respuesta) => {
+    let mascota = {
+        nombre: "Maggie",
+        edad: 2,
+    };
+    respuesta.json(mascota);
+});
+
+// Una vez definidas nuestras rutas podemos iniciar el servidor
+app.listen(puerto, err => {
+    if (err) {
+        // Aquí manejar el error
+        console.error("Error escuchando: ", err);
+        return;
+    }
+    // Si no se detuvo arriba con el return, entonces todo va bien ;)
+    console.log(`Escuchando en el puerto :${puerto}`);
+});
